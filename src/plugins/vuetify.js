41c51230aac1135c6b0fd9933f56778cce2c1366
Bogdan-t <user332145@gmail.com>
2020-09-29 20:26:13 +0300

    added ukrainian locale to vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
+import uk from 'vuetify/es5/locale/uk';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  lang: {
+    locales: { uk },
+    current: 'uk',
+  },
   icons: {
     iconfont: 'mdiSvg',
   },

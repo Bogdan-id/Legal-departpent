72e69bd08fc05be0807ae03927763dc3a63513b5
bogdan-t <user332145@gmail.com>
2021-04-27 08:03:59 +0300

    added "tree.vue" component

---
+++ src/components/legal/tree.vue
@@ -0,0 +1,27 @@
+<template>
+<v-card 
+  v-if="legal.code"
+  :min-width="750"
+  style="position: absolute; top: 250px; z-index: 500">
+  <v-card-text class="text-left">
+    <strong>{{ "Компанiя: " + legal.name.shortName }}</strong>
+    <Legal 
+      :legal="legal">
+    </Legal>
+  </v-card-text>
+</v-card>
+</template>
+<script>
+// import Founder from './founder.vue'
+import Legal from './legal.vue'
+export default {
+  components: {
+    // Founder,
+    Legal,
+  },
+  props: {legal: Object},
+  mounted() {
+    console.log('LEGAL', this.legal)
+  },
+}
+</script>

ac51b17dac24c8188ced9ce2354c21213ab2881b
bogdan-t <user332145@gmail.com>
2021-04-30 22:32:06 +0300

    added axios to global object

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
+'use-strict'
+// @ts-check
 import Vue from 'vue'
+// @ts-ignore
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import Snotify from 'vue-snotify'
+import storeInstance from './store/index'
+import Vuex from 'vuex'
+import axios from './plugins/axios'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
 Vue.use(Snotify)
+const store = new Vuex.Store(storeInstance)
+
+const vm = Vue.prototype
+vm.$axios = axios(store)
+vm.$store = store
 
 new Vue({
   vuetify,

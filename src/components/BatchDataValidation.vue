0b549ff7faeb4fa662e3a294cb275d951136378b
bogdan-t <user332145@gmail.com>
2021-06-26 12:27:32 +0300

    fixed baseURL

--- src/components/BatchDataValidation.vue
+++ src/components/BatchDataValidation.vue
@@ -470,7 +470,7 @@ export default {
       this.dwn(this.global, { entity: this.entity })
     },
     setBaseUrl() {
-      this.baseUrl = process.env.NODE_ENV === "development" 
+      this.baseURL = process.env.NODE_ENV === "development" 
         ? 'http://127.0.0.1:4000'
         : 'http://94.131.243.7:4000'
     },

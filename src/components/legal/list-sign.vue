5f540f591c2cbed161f7bbbab01818ae520f8809
bogdan-t <user332145@gmail.com>
2021-07-01 16:41:45 +0300

    hot fix

--- src/components/legal/list-sign.vue
+++ src/components/legal/list-sign.vue
@@ -35,7 +35,7 @@
           </div> 
           <div>
             <span class="info-label">Тiло запиту:&nbsp;&nbsp;</span> 
-            <span class="info-data">{{ config.data ? JSON.parse(config.data) : config.url }}</span>
+            <span class="info-data">{{ (config.data !== null || config.data !== 'undefined') ? JSON.parse(config.data) : config.url }}</span>
           </div> 
           <div>
             <span class="info-label">Метод:&nbsp;&nbsp;</span> 
